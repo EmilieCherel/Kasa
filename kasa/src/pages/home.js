c00738da4c2a8e3c8ed5f9f3fad9695baca3907f
EmilieCherel <cherel.emi@gmail.com>
2023-02-01 20:07:30 +0100

    Mise en place et stylisation de la bannière de la page d'accueil

--- kasa/src/pages/home.js
+++ kasa/src/pages/home.js
@@ -1,21 +1,45 @@
 import styled from 'styled-components';
+import HomeBannerImg from '../assets/HomeBanner.png';
 
 const HomeWrapper = styled.div`
   display: flex;
   justify-content: center;
 `;
 
-const HomeContainer = styled.div`
-  margin: 30px;
-  display: flex;
-  flex-direction: row;
-  max-width: 1200px;
+const HomeContainer = styled.div``;
+
+const HomeBanner = styled.img`
+  position: relative;
+  filter: brightness(70%);
+  border-radius: 25px;
+`;
+
+const HomeTitle = styled.h1`
+  position: absolute;
+  left: 50%;
+  top: 25%;
+  transform: translate(-50%, -50%);
+  font-size: 48px;
+  font-weight: 500;
+  color: white;
+`;
+
+const HomeGallery = styled.div`
+  margin: 40px 0px;
+  width: 1240px;
+  height: 829px;
+  background: #f7f7f7;
+  border-radius: 25px;
 `;
 
 function Home() {
   return (
     <HomeWrapper>
-      <HomeContainer></HomeContainer>
+      <HomeContainer>
+        <HomeBanner src={HomeBannerImg} />
+        <HomeTitle>Chez vous, partout et ailleurs</HomeTitle>
+        <HomeGallery></HomeGallery>
+      </HomeContainer>
     </HomeWrapper>
   );
 }
